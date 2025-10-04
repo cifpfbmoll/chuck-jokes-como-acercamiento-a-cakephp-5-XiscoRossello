b73ee30b6d84e0d050d5f94d396b5d00a2bafbad
XiscoRossello <frossellojeronimo@cifpfbmoll.eu>
2025-10-04 18:29:23 +0200

    docs: añadir capturas

--- config/routes.php
+++ config/routes.php
@@ -51,14 +51,12 @@ return function (RouteBuilder $routes): void {
 
     $routes->scope('/', function (RouteBuilder $builder): void {
         /*
-         * Here, we are connecting '/' (base path) to a controller called 'Pages',
-         * its action called 'display', and we pass a param to select the view file
-         * to use (in this case, templates/Pages/home.php)...
+         * Ruta principal: redirige directamente a Chuck Norris Jokes
          */
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+        $builder->connect('/', ['controller' => 'Jokes', 'action' => 'random']);
 
         /*
-         * ...and connect the rest of 'Pages' controller's URLs.
+         * Mantenemos las páginas por si se necesitan
          */
         $builder->connect('/pages/*', 'Pages::display');
 
